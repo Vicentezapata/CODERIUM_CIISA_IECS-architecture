67466d3db418e2f8e8b5b8be8f92dd07f0dad9b1
Vicente Zapata <vicente.zapata@usach.cl>
2021-07-13 23:45:07 -0400

    update 13-07 - clean folders

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 sudo sysctl -w vm.max_map_count=262144
-#sudo mkdir clairscanner
-#sudo wget https://github.com/arminc/clair-scanner/releases/download/v12/clair-scanner_linux_amd64
-#sudo mv clair-scanner_linux_amd64 clairscanner/clair-scanner
-#sudo chmod +x clairscanner/clair-scanner
 sudo docker-compose up -d
